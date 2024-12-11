04831ecaf2756cbf8cca7650e9d48874bd794ab1
Carter2565 <2565tube@gmail.com>
2024-12-11 15:34:05 -0700

    modified:   docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,8 @@
 # Configuration file for the Sphinx documentation builder.
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))  # Adjust the path as needed
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../../'))  # Adjust the path as needed
 
 # -- Project information
 
