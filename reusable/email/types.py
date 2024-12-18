de33306036efc78dcc1b9b7f11aaf5d7aa645b4b
Carter2565 <2565tube@gmail.com>
2024-12-18 14:22:31 -0700

    Minor updates

--- reusable/email/types.py
+++ reusable/email/types.py
@@ -11,7 +11,7 @@ class Email:
 
   @classmethod
   def from_json(cls, decrypted_email_data: str):
-    # Parse JSON and create an Email instance
+    '''Parse JSON and create an Email instance'''
     email_json = json.loads(decrypted_email_data)
     return cls(
       id=email_json.get('id', None),
@@ -21,15 +21,15 @@ class Email:
       body=email_json.get('body', None)
     )
   
-  @classmethod
-  def to_json(cls, email_instance):
-    # Convert Email instance to JSON string
+  @staticmethod
+  def to_json(email_instance):  
+    '''Convert Email instance to JSON string'''
     email_dict = {
-      'id': email_instance.id,
-      'subject': email_instance.subject,
-      'sender': email_instance.sender,
-      'timestamp': email_instance.timestamp,
-      'body': email_instance.body
+        'id': email_instance.id,
+        'subject': email_instance.subject,
+        'sender': email_instance.sender,
+        'timestamp': email_instance.timestamp,
+        'body': email_instance.body
     }
     return json.dumps(email_dict)
 
